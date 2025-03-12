3a0c2d80f851ef115beef1cec9ea949362b0d031
laurenCho9 <lauren.choux@gmail.com>
2025-03-13 01:55:46 +0900

    docs: 주석 추가

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -19,6 +19,7 @@ export default DynamicRouter;
 // 이 기능은 동적 경로 세그먼트generateStaticParams 와 함께 사용하여
 // 요청 시점에 주문형으로 생성하는 대신 빌드 시점에 정적으로 경로를 생성
 export async function generateStaticParams() {
+  // 먼저 SSG를 만들고 싶을 때 배열로 추가
   const products = ["women", "man", "pants", "skirt"];
 
   return products.map((product) => ({
