c9269459b1d0a3f8496ac54e3009202c5dc296e9
laurenCho9 <lauren.choux@gmail.com>
2025-04-04 07:55:18 +0900

    클라이언트 컴포넌트 사용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import { Counter } from "@/components/Counter";
+import os from "os"; // 노드 APIs
+
 export default function Home() {
+  console.log("안녕?");
+  console.log(os.hostname());
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      홈페이지 메인 ❗️
-    </div>
+    <>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        홈페이지 메인 ❗️
+      </div>
+      <Counter />
+    </>
   );
 }
